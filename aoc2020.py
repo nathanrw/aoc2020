f6bef09da5d7564651e59b8fc7a592d023a8ea62
Nathan Woodward <nathanrichardwoodward@gmail.com>
2020-12-20 20:26:17 +0000

    day 2

--- aoc2020.py
+++ aoc2020.py
@@ -1,5 +1,5 @@
 import argparse
-
+import re
 
 def task_1_part_1():
     with open('input1.txt') as f:
@@ -32,9 +32,52 @@ def task_1_part_2():
     print(number)
 
 
+def task_2_part_1():
+    with open('input2.txt') as f:
+        lines = f.readlines()
+    class Record(object):
+        def __init__(self, line):
+            pattern = "(\\d+)-(\\d+) (\\w): (\\w+)"
+            match = re.search(pattern, line)
+            assert match
+            self.rule_min_count = int(match.group(1))
+            self.rule_max_count = int(match.group(2))
+            self.rule_character = match.group(3)
+            self.password = match.group(4)
+        def is_valid(self):
+            count = self.password.count(self.rule_character)
+            return self.rule_min_count <= count and count <= self.rule_max_count
+    records = [ Record(line) for line in lines ]
+    count = len([record for record in records if record.is_valid()])
+    print(count)
+
+
+def task_2_part_2():
+    with open('input2.txt') as f:
+        lines = f.readlines()
+    class Record(object):
+        def __init__(self, line):
+            pattern = "(\\d+)-(\\d+) (\\w): (\\w+)"
+            match = re.search(pattern, line)
+            assert match
+            self.rule_index_1 = int(match.group(1))-1
+            self.rule_index_2 = int(match.group(2))-1
+            self.rule_character = match.group(3)
+            self.password = match.group(4)
+        def is_valid(self):
+            index_1_matches = self.password[self.rule_index_1] == self.rule_character
+            index_2_matches = self.password[self.rule_index_2] == self.rule_character
+            return index_1_matches != index_2_matches
+    records = [ Record(line) for line in lines ]
+    count = len([record for record in records if record.is_valid()])
+    print(count)
+
+
 def main():
-    task_1_part_1()
-    task_1_part_2()
+    #task_1_part_1()
+    #task_1_part_2()
+    task_2_part_1()
+    task_2_part_2()
 
 
 if __name__ == '__main__':
